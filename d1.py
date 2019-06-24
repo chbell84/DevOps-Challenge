4be8053e5d09ee3cf7a52dd71476aa1b12973735
Charles Bell <15733651+chbell84@users.noreply.github.com>
2019-06-24 01:44:34 -0400

    made changes for Python 3. Fixeda rounding error

--- d1.py
+++ d1.py
@@ -7,22 +7,16 @@
 import argparse
 import csv
 import xlsxwriter
+import math
 
 #Reads in CSV file returns an array
 def loadcsv(file):
     dataset = []
-    with open(file, 'rb') as csvfile:
+    with open(file) as csvfile:
         datareader = csv.reader(csvfile, delimiter=',', quotechar='"')
         dataset = [row for row in datareader]
         #dataset = [row[0].split(',') for row in dataset]
         return dataset
-
-def makeDict(list):
-    excelDict = {}
-    for i in range(len(list[0])):
-        excelDict.update({list[0][i] : [list[j+1][i] for j in range(len(list)-1)]})
-        print (list[0][i])
-    return excelDict
 
 # Saves a excel  version of the list with the columns in reverse order
 def reverseSave(filename, data):
@@ -38,10 +32,9 @@ def alterSave(filename, data):
     workbook = xlsxwriter.Workbook(filename)
     worksheet = workbook.add_worksheet()
     for i in range(len(data)):
-        for j in range(len(data[0])/2):
+        for j in range(math.ceil(len(data[0])/2)):
             worksheet.write(i,j,data[i][j*2])
     workbook.close()
-
 
 testFile = 'train.csv'
 data = loadcsv(testFile)
