c500e5621131faf057f5331ca231713e5123917f
Charles Bell <15733651+chbell84@users.noreply.github.com>
2019-07-01 15:42:55 -0400

    added file upload

--- app/routes.py
+++ app/routes.py
@@ -2,35 +2,54 @@ import os
 import csv
 import xlsxwriter
 import math
-from flask import render_template, send_file, url_for
+from flask import Flask, request, render_template, send_file
 from app import app
+from werkzeug.utils import secure_filename
 
-@app.route('/')
+#DEFAULT_FILE = os.path.join(app.config['UPLOAD_FOLDER_SF'],'default.csv')
+
+@app.route('/', methods = ['GET', 'POST'])
 @app.route('/index')
 def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+        file.save(filepath)
+        return load_csv(filename)
     return render_template('index.html',  title='Home')
 
 @app.route('/data/')
-def return_files(filename='static/files/default.csv'):
+@app.route('/data/<filename>')
+def return_files(filename=None):
+    if filename is None:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER_SF'],'default.csv')
+    else:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER_SF'],secure_filename(filename))
     try:
-        return send_file(filename)
+        return send_file(filepath)
     except Exception as e:
         return str(e)
 
 #Reads in CSV file returns an array
-@app.route('/load-csv')
-def load_csv(file='app/static/files/default.csv'):
+@app.route('/load-csv/')
+@app.route('/load-csv/<filename>')
+def load_csv(filename=None):
+    if filename is None:
+        file = os.path.join(app.config['UPLOAD_FOLDER'],'default.csv')
+    else:
+        file = os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(filename))
     dataset = []
     with open(file) as csvfile:
         datareader = csv.reader(csvfile, delimiter=',', quotechar='"')
         dataset = [row for row in datareader]
     reverse_save(dataset)
     alter_save(dataset)
-    return render_template('load-csv.html',  title='Results')
+    return render_template('load-csv.html',  title='File Downloads')
 
 # Saves a excel  version of the list with the columns in reverse order
-def reverse_save(data, filename='app/static/files/reversed_columns.xlsx'):
-    workbook = xlsxwriter.Workbook(filename)
+def reverse_save(data, file=os.path.join(app.config['UPLOAD_FOLDER'],'reversed_columns.xlsx')):
+    workbook = xlsxwriter.Workbook(file)
     worksheet = workbook.add_worksheet()
     for i in range(len(data)):
         for j in range(len(data[0])):
@@ -38,8 +57,8 @@ def reverse_save(data, filename='app/static/files/reversed_columns.xlsx'):
     workbook.close()
 
 # Saves a excel version of the list with every other column ommited
-def alter_save(data, filename='app/static/files/alternate_columns.xlsx'):
-    workbook = xlsxwriter.Workbook(filename)
+def alter_save(data, file=os.path.join(app.config['UPLOAD_FOLDER'],'alternate_columns.xlsx')):
+    workbook = xlsxwriter.Workbook(file)
     worksheet = workbook.add_worksheet()
     for i in range(len(data)):
         for j in range(math.ceil(len(data[0])/2)):
