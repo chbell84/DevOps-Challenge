a9dddcc0bbff3ac9e77fdb049a5fde86cbf69f4d
Charles Bell <15733651+chbell84@users.noreply.github.com>
2019-07-03 02:49:30 -0400

    added a function

--- d4.sh
+++ d4.sh
@@ -3,28 +3,43 @@ OLDIFS=$IFS
 IFS=$'\n\r'
 array=()
 l=$( sed 's/\(.*,\)\("[^,]*\),\([^,]*"\)\(.*\)/\1\2|\3\4/' file )
-for line in $l;
-do
-    IFS=$','
-    outLine=$''
-    for i in $line; do
-#        tempIFS=$IFS
-        IFS=$'\n'
-#    read -ra i<<<"$i"
-        i="${i/|/,}"
-        if [[ -z "$outLine" ]]; then
-            outLine=$i
-#        echo $outLine
-#        echo $i
-        else
-            outLine=$i','$outLine
-#            echo $outLine
-        fi
-#    echo $i
-#        IFS=$tempIFS
-#        echo $outLine
+
+reverse_columns() {
+    IFS=$'\n\r'
+    for line in $@;
+    do
+        IFS=$','
+        outLine=$''
+        for i in $line; do
+            IFS=$'\n'
+            i="${i/|/,}"
+            if [[ -z "$outLine" ]]; then
+                outLine=$i
+            else
+                outLine=$i','$outLine
+            fi
+        done
+        echo $outLine
     done
-    echo $outLine
-done>>outfile.csv
+}
+
+#echo $l
+reverse_columns $l > reversed_columns.csv
+#for line in $l;
+#do
+#    IFS=$','
+#    outLine=$''
+#    for i in $line; do
+#        IFS=$'\n'
+#        i="${i/|/,}"
+#        if [[ -z "$outLine" ]]; then
+#            outLine=$i
+#        else
+#            outLine=$i','$outLine
+#        fi
+#    done
+#    echo $outLine
+#done>reversed_columns.csv
+
 #done
 IFS=$OLDIFS
